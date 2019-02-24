debfe8e5df24cd32bb26a27db748718ec0a49ace
Carlile Upfront <carlileupfront@users.noreply.github.com>
2019-02-24 12:16:38 -0600

    Add project files.

---
+++ Lesson5_Methods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson5_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
