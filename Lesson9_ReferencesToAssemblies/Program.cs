819414ad51ac639a1b7e30194668cdce10c2a8e3
CailieAC <cailiecarlile@gmail.com>
2022-01-09 21:45:55 -0600

    Finished Lesson 9 References to Scrape Webpages

--- Lesson9_ReferencesToAssemblies/Program.cs
+++ Lesson9_ReferencesToAssemblies/Program.cs
@@ -6,7 +6,17 @@ namespace Lesson9_ReferencesToAssemblies
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter URL: ");
+            string url = Console.ReadLine();
+            Console.Write("Enter filepath: ");
+            string filepath = Console.ReadLine();
+            ScrapeLibrary newScrape = new ScrapeLibrary();
+
+            if (filepath.Length > 0)
+                Console.WriteLine(newScrape.ScrapeWebpage(url, filepath));
+            else
+                Console.WriteLine(newScrape.ScrapeWebpage(url));
+            Console.ReadLine();
         }
     }
 }
---
+++ Lesson9_ReferencesToAssemblies/ScrapeLibrary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Lesson9_ReferencesToAssemblies
+{
+    class ScrapeLibrary
+    {
+        public string ScrapeWebpage(string url)
+        {
+            return GetWebpage(url);
+        }
+        public string ScrapeWebpage(string url, string filepath)
+        {
+            string reply = GetWebpage(url);
+            File.WriteAllText(filepath, reply);
+            return reply;
+        }
+
+        private string GetWebpage(string url)
+        {
+            WebClient client = new WebClient();
+            string content = client.DownloadString(url);
+            content += "That's All Folks!";
+            return content;
+        }
+    }
+}
